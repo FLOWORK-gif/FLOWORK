bda817050bd6b2b190c68e5cf6acd433655330eb
awenkaudico <mraolasahidin2402@gmail.com>
2025-09-02 11:46:20 +0700

    feat: update 151 file(s)

---
+++ flowork_kernel/core/build_security.py
@@ -0,0 +1,20 @@
+#######################################################################
+# dev : awenk audico
+# EMAIL SAHIDINAOLA@GMAIL.COM
+# WEBSITE WWW.TEETAH.ART
+# File NAME : C:\FLOWORK\flowork_kernel\core\build_security.py
+# (This is a new file to be created)
+# NOTE: This file holds our security signature and runtime checks.
+#######################################################################
+import os
+import sys
+
+# (COMMENT) Signature logic is disabled as per user request.
+OFFICIAL_BUILD_SIGNATURE = ""
+
+def perform_runtime_check(module_file_path):
+    """
+    (COMMENT) This security check is currently disabled as per user request.
+    The function is kept to prevent import errors but does nothing.
+    """
+    pass
